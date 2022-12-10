e7a56025783a34f8f8fc22365304a079201af5a4
takumi-1016 <tn1016recruit@gmail.com>
2022-12-10 16:13:40 +0900

    投稿機能

--- app/Http/Controllers/Tweet/CreateController.php
+++ app/Http/Controllers/Tweet/CreateController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers\Tweet;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\Tweet\CreateRequest;
+use App\Models\Tweet;
 
 class CreateController extends Controller
 {
@@ -13,8 +14,12 @@ class CreateController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request)
+    public function __invoke(CreateRequest $request)
     {
-        //
+        $tweet = new Tweet;
+        $tweet->content = $request->tweet();
+        $tweet->save();
+        return redirect()->route('tweet.index');
     }
+
 }
